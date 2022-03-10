b31fc981c500daa869e72e90a54d22bb651b8159
VinitaBhatt <71602249+VinitaBhatt@users.noreply.github.com>
2022-03-10 16:47:08 +0530

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import {View,Text,StyleSheet,SafeAreaView,StatusBar,Platform,TouchableOpacity} from 'react-native';
+
+
+export default class HomeScreen extends Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea}/>
+                <View style={styles.titleBar}>
+                    <Text style={styles.titleText}> ISS Tracker </Text>
+                </View>
+
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style={styles.routeText}>
+                        ISS Location 
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.routeCard}>
+                    <Text style={styles.routeText}>
+                        Meteors 
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"orange"
+    },
+    titleBar:{
+        flex:0.25,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    titleText:{
+        fontSize:40,
+        fontWeight:'bold',
+        color:'white'      
+    },
+    droidSafeArea:{
+        marginTop:Platform.OS==="android" ? StatusBar.currentHeight:0
+    },
+    routeCard:{
+        flex:0.25,
+        marginLeft:50,
+        marginTop:50,
+        marginRight:50,
+        borderRadius:30,
+        backgroundColor:"white"
+    },
+    routeText:{
+        fontSize:35,
+        fontWeight:"bold",
+        color:"black",
+        marginTop:75,
+        paddingLeft:30
+    }
+})
